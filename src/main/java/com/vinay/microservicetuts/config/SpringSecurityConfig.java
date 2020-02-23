65b9fbe64fe46e6b85fb9cac557427451ef41a65
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-02-23 15:05:18 +0530

    Spring Security With Basic Authentication And In memory authentication added

---
+++ src/main/java/com/vinay/microservicetuts/config/SpringSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.vinay.microservicetuts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("vinay").password("drago").roles("USER")
+                .and()
+                .withUser("admin").password("adminpassword").roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests().antMatchers("/**").authenticated().
+                and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
